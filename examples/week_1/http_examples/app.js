88ccabc5a29565d9f4893916d1e8d23671ff1859
Brian Arayathel <brian.arayathel@revature.com>
2024-09-12 10:15:56 -0400

    http basic api example

--- examples/week_1/http_examples/app.js
+++ examples/week_1/http_examples/app.js
@@ -22,8 +22,54 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
     // request handling logic
-    res.writeHead(200, { 'Content-Type': 'text/plain'});
-    res.end("Hello, World!");
+
+    logger.info(`[${req.method} ${req.url}]`);
+
+    res.setHeader('Content-Type', 'application/json');
+
+    // handle the different HTTP methods
+    let body = '';
+    switch(req.method){
+        case('GET'):
+            res.statusCode = 200;
+            res.end(JSON.stringify({message: "GET request handled"}))
+            break;
+        case('POST'):
+            // expects there to be a body
+            body = '';
+            req.on('data', (chunk) => {
+                body += chunk;
+            });
+            req.on('end', () => {
+                logger.info(`Request body: ${body}`);
+                res.statusCode = 201; // created
+                res.end(JSON.stringify({message: "POST request handled"}));
+            });
+            break;
+        case('PUT'):
+            // expects there to be a body
+            body = '';
+            req.on('data', (chunk) => {
+                body += chunk;
+            });
+            req.on('end', () => {
+                logger.info(`Request body: ${body}`);
+                res.statusCode = 200; 
+                res.end(JSON.stringify({message: "PUT request handled"}));
+            });
+            break;
+        case('DELETE'):
+            res.statusCode = 200;
+            res.end(JSON.stringify({message: "DELETE request handled"}))
+            break;
+        default:
+            res.statusCode = 405; // method not allowed
+            res.end(JSON.stringify({message: "Method not supported"}))
+            break;
+    }
+
+    // res.writeHead(200, { 'Content-Type': 'text/plain'});
+    // res.end("Hello, World!");
 });
 
 server.listen(PORT, () => {
