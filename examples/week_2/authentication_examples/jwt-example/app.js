4b773e49700b3cf51c0684fdbec484f3886254e2
Brian Arayathel <brian.arayathel@revature.com>
2024-09-20 10:19:24 -0400

    jwt bcrypt example

--- examples/week_2/authentication_examples/jwt-example/app.js
+++ examples/week_2/authentication_examples/jwt-example/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const app = express();
 const PORT = 3000;
@@ -13,7 +14,12 @@ const secretKey = "your-secret-key";
 const users = [];
 
 app.post("/register", async (req, res) => {
-    const {username, password, role} = req.body;
+    let {username, password, role} = req.body;
+
+    const saltRounds = 10;
+
+    password = await bcrypt.hash(password, saltRounds);
+    console.log(password);
 
     const newUser = { id: users.length + 1, username, password, role};
 
@@ -29,7 +35,7 @@ app.post("/login", async (req, res) => {
     // find the user in the database
     const user = users.find((user) => user.username === username);
     
-    if (!user || user.password !== password){
+    if (!user || !(await bcrypt.compare(password, user.password))){
         res.status(401).json({message: "Invalid Credentials"});
     }else{
         // generate the JWT token
@@ -49,6 +55,55 @@ app.post("/login", async (req, res) => {
         res.json({token});
     }
 });
+
+app.get("/protected", authenticateToken, (req, res) => {
+    res.json({message : "Protected Route Accessed", user: req.user});
+});
+
+app.get("/admin-protected", authenticateAdminToken, (req, res) => {
+    res.json({message : "Protected Admin Route Accessed", user: req.user});
+});
+
+async function authenticateToken(req, res, next){
+    // authorization: "Bearer tokenstring"
+
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (!token){
+        res.status(401).json({message: "Unauthorized Access"});
+    }else{
+        const user = await decodeJWT(token);
+        req.user = user;
+        next();
+    }
+}
+
+async function authenticateAdminToken(req, res, next){
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (!token){
+        res.status(401).json({message: "Unauthorized Access"});
+    }else{
+        const user = await decodeJWT(token);
+        if (user.role !== "admin"){
+            res.status(403).json({message: "Forbidden Access"});
+            return;
+        }
+        req.user = user;
+        next();
+    }
+}
+
+async function decodeJWT(token){
+    try{
+        const user = await jwt.verify(token, secretKey)
+        return user;
+    }catch(err){
+        console.error(err);
+    }
+}
 
 app.listen(PORT, () => {
     console.log("Server is listening on http://localhost:3000");
